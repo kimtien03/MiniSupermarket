89007285aec581179f6212ce05c2e7cc3a0ddb3e
tn6609092@gmail.com <tn6609092@gmail.com>
2023-10-21 15:18:58 +0100

    loadPN

--- src/com/myproject/BUS/PhieuNhapBUS.java
+++ src/com/myproject/BUS/PhieuNhapBUS.java
@@ -1,5 +1,13 @@
 package com.myproject.BUS;
 
+import com.myproject.DAO.PhieuNhapDAO;
+import com.myproject.DTO.PhieuNhapDTO;
+import java.util.ArrayList;
+
 public class PhieuNhapBUS {
-    
+    PhieuNhapDAO pn;
+    public ArrayList<PhieuNhapDTO> getAllPN() {
+        pn = new PhieuNhapDAO();
+        return pn.getAllPN();
+    }
 }
--- src/com/myproject/DAO/PhieuNhapDAO.java
+++ src/com/myproject/DAO/PhieuNhapDAO.java
@@ -1,7 +1,35 @@
 package com.myproject.DAO;
 
-import com.myproject.BUS.*;
+import com.myproject.DTO.PhieuNhapDTO;
+import java.security.Timestamp;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class PhieuNhapDAO extends conndb{
-    
+    public ArrayList<PhieuNhapDTO> getAllPN() {
+       ArrayList<PhieuNhapDTO> arr = new ArrayList<>();
+       if(openConnection()) {
+           try {
+               String sql = "SELECT * FROM PHIEUNHAP";
+               Statement s = con.createStatement();
+               ResultSet rs = s.executeQuery(sql);
+               System.out.println(rs.next());
+               while(rs.next()) {
+                   PhieuNhapDTO p = new PhieuNhapDTO();
+                   p.setMaPN(rs.getString("MaPhieuNhap"));
+                   p.setMaNV(rs.getString("MaNV"));
+                   p.setNgLapPhieu(rs.getTimestamp("NgLapPhieu"));
+                   p.setThanhTien(rs.getFloat("ThanhTien"));
+                   p.setTinhTrang(rs.getBoolean("TinhTrang"));
+                   arr.add(p);
+               }
+           } catch (Exception e) {
+               System.out.println(e);
+           } finally {
+               closeConnection();
+           }
+       }
+       return arr;
+   }
 }
