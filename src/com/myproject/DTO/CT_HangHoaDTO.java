2ef303336d0fc5280886ae469d8aa868e51b22ea
kimtien03 <volekimtien@gmail.com>
2023-10-21 10:41:22 +0700

    a

---
+++ src/com/myproject/DTO/CT_HangHoaDTO.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.DTO;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author ACER
+ */
+public class CT_HangHoaDTO {
+    private String MaCT_HH;
+    private String MaHH;
+    private Timestamp NgaySX;
+    private Timestamp HSD;
+    private float SoLuong;
+    private boolean TinhTrang;
+    public CT_HangHoaDTO(String MaCT_HH, String MaHH,  Timestamp NgaySX,Timestamp HSD, float SoLuong,  boolean TinhTrang) {
+        this.MaHH = MaHH;
+        this.MaHH = MaHH;
+        this.NgaySX = NgaySX;
+        this.HSD = HSD;
+        this.SoLuong = SoLuong;
+        this.TinhTrang = TinhTrang;
+    }
+
+    public String getMaCT_HH() {
+        return MaCT_HH;
+    }
+
+    public void setMaCT_HH(String MaCT_HH) {
+        this.MaCT_HH = MaCT_HH;
+    }
+
+    public String getMaHH() {
+        return MaHH;
+    }
+
+    public void setMaHH(String MaHH) {
+        this.MaHH = MaHH;
+    }
+
+    public Timestamp getNgaySX() {
+        return NgaySX;
+    }
+
+    public void setNgaySX(Timestamp NgaySX) {
+        this.NgaySX = NgaySX;
+    }
+
+    public Timestamp getHSD() {
+        return HSD;
+    }
+
+    public void setHSD(Timestamp HSD) {
+        this.HSD = HSD;
+    }
+
+    public float getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(float SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public boolean isTinhTrang() {
+        return TinhTrang;
+    }
+
+    public void setTinhTrang(boolean TinhTrang) {
+        this.TinhTrang = TinhTrang;
+    }
+}
