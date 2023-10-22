dc597b4c8e1c103763b8bcb439456f36e3538cef
tiendatnguyenvu <nguyenvutiendat07022003@gmail.com>
2023-10-22 15:06:40 +0700

    tui lam tren nhanh chinh luon

--- src/com/myproject/DAO/NhanVienDAO.java
+++ src/com/myproject/DAO/NhanVienDAO.java
@@ -1,7 +1,16 @@
 package com.myproject.DAO;
 
 import com.myproject.BUS.*;
+import com.myproject.DTO.NhanVienDTO;
+import java.util.ArrayList;
 
 public class NhanVienDAO extends conndb{
-    
+    // Lấy toàn bộ danh sách nhân viên
+    public ArrayList<NhanVienDTO> getAllStaffInfoAsArraylist() {
+        ArrayList<NhanVienDTO> staffArrayList = new ArrayList<NhanVienDTO>();
+        
+        
+        
+        return staffArrayList;
+    }
 }
