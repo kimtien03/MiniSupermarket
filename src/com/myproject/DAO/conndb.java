1e6450c317515d038d7fac1e8c6283d96278153a
tn6609092@gmail.com <tn6609092@gmail.com>
2023-10-29 11:43:10 +0100

    moi

--- src/com/myproject/DAO/conndb.java
+++ src/com/myproject/DAO/conndb.java
@@ -11,7 +11,7 @@ public class conndb {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String dbUrl = "jdbc:sqlserver://localhost:1433;databaseName=SIEUTHIMINI;encrypt=true;trustServerCertificate=true";
             String username = "sa";
-            String password = "123456789";
+            String password = "123456";
             con = DriverManager.getConnection(dbUrl,username,password);
             return true;
         } catch (Exception e) {
