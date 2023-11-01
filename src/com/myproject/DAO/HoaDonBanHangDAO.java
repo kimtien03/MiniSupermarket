042d480e716b358252440d0de9827652f2dcde2e
tn6609092@gmail.com <tn6609092@gmail.com>
2023-11-01 18:43:00 +0100

    Merge Admin_TienDat to master

--- src/com/myproject/DAO/HoaDonBanHangDAO.java
+++ src/com/myproject/DAO/HoaDonBanHangDAO.java
@@ -13,29 +13,28 @@ import java.util.List;
 
 public class HoaDonBanHangDAO extends conndb {
 
-    conndb ConHDBH = new conndb();
     public List<HoaDonBanHangDTO> getList() {
         List<HoaDonBanHangDTO> list = new ArrayList<>();
-        try {
-            ConHDBH.openConnection();
-            Connection cons = ConHDBH.getConnection();
-            String sql = "SELECT*FROM HOADONBANHANG";
-            PreparedStatement ps = cons.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                HoaDonBanHangDTO HDBanHang = new HoaDonBanHangDTO();
-                HDBanHang.setMaHD(rs.getString("MaHD"));
-                HDBanHang.setNgLap(rs.getTimestamp("NgLapHD"));
-                HDBanHang.setThanhTien(rs.getFloat("ThanhTien"));
-                HDBanHang.setMaKH(rs.getString("MaKH"));
-                HDBanHang.setMaNV(rs.getString("MaNV"));
-                list.add(HDBanHang);
+        if (openConnection()) {
+            try {
+                String sql = "SELECT*FROM HOADONBANHANG";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    HoaDonBanHangDTO HDBanHang = new HoaDonBanHangDTO();
+                    HDBanHang.setMaHD(rs.getString("MaHD"));
+                    HDBanHang.setNgLap(rs.getTimestamp("NgLapHD"));
+                    HDBanHang.setThanhTien(rs.getFloat("ThanhTien"));
+                    HDBanHang.setMaKH(rs.getString("MaKH"));
+                    HDBanHang.setMaNV(rs.getString("MaNV"));
+                    list.add(HDBanHang);
+                }
+                ps.close();
+                rs.close();
+                return list;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            ps.close();
-            rs.close();
-            return list;
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return null;
     }
